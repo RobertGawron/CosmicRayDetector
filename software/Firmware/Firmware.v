c0e500f6a3476fe7c6c9ca2627fe2aa6057a3666
Robert Gawron <robert_gawron@yahoo.fr>
2019-08-31 12:51:20 +0300

    added main block diagram

--- software/Firmware/Firmware.v
+++ software/Firmware/Firmware.v
@@ -1,7 +1,30 @@
-module Firmware (
-	input clk
-
+module Firmware(
+	input x,
+	input y,
+	output z);
  
-);
+	assign  z = x & y;
 
 endmodule
+
+
+
+module stimulus;
+
+    reg x;
+    reg y;
+    wire z;
+    
+    comparator uut (
+        .x(x),
+        .y(y),
+        .z(z)
+    );
+        
+        
+    initial begin
+        $monitor("x=%d,y=%d,z=%d \n",x,y,z);
+    end
+    
+endmodule
+
